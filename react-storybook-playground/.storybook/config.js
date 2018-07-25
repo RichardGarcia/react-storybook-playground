a4cb4d0d6afe2c1b6bb74967736ab07b4c780e2f
Richard Garcia <richardgarciaph@gmail.com>
2018-07-25 12:33:39 +1200

    delete dead files..

--- react-storybook-playground/.storybook/config.js
+++
@@ -1,8 +0,0 @@
-import { configure } from '@storybook/react'
-import { configureViewport } from '@storybook/addon-viewport'
-
-function loadStories () {
-  require('../src/stories')
-}
-
-configure(loadStories, module)
