b522592ad2e2516edb709a91343c839744ae41c8
Richard Garcia <richardgarciaph@gmail.com>
2018-07-25 15:47:34 +1200

    add viewport addons

--- react-storybook-playground/.storybook/config.js
+++ react-storybook-playground/.storybook/config.js
@@ -1,7 +1,9 @@
-import { configure } from '@storybook/react';
+import { configure } from '@storybook/react'
+import { configureViewport } from '@storybook/addon-viewport'
+// import { configureViewport, INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 
-function loadStories() {
-  require('../src/stories');
+function loadStories () {
+  require('../src/stories')
 }
 
-configure(loadStories, module);
+configure(loadStories, module)
