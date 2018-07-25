ed90f40b8b713fdf29a0833535200cc5ad6b3cee
Richard Garcia <richardgarciaph@gmail.com>
2018-07-25 13:22:31 +1200

    connect react app to storybook and add custom app title sample

--- react-storybook-playground/src/App.js
+++ react-storybook-playground/src/App.js
@@ -1,21 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
+    // App title props
+    const { appTitle } = this.props
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1 className='App-title'>Welcome to {appTitle}</h1>
         </header>
-        <p className="App-intro">
+        <p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+// add default title in case nothing is passed
+App.defaultProps = {
+  appTitle: 'React'
+}
+
+export default App
--- react-storybook-playground/src/stories/index.js
+++ react-storybook-playground/src/stories/index.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React from 'react'
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import { linkTo } from '@storybook/addon-links'
 
-import { Button, Welcome } from '@storybook/react/demo';
+import { Button, Welcome } from '@storybook/react/demo'
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+// import your react app here
+import App from '../App'
+
+storiesOf('Welcome', module).add('to Storybook', () => (
+  <Welcome showApp={linkTo('Button')} />
+))
 
 storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+  .add('with text', () => (
+    <Button onClick={action('clicked')}>Hello Button</Button>
+  ))
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
+  ))
+
+// load your react app
+storiesOf('React Storybook Playground App', module)
+  .add('Default', () => <App />)
+  .add('Custom app title', () => <App appTitle='React Storybook Playground' />)
